8dec874c641c21b687cc9f66ca12dc66e4ed1323
anupama <anupamay701@gmail.com>
2021-09-07 18:34:35 +0530

    Slf4j project setup

---
+++ src/main/java/com/slf4jlogging/Slf4jloggingApplication.java
@@ -0,0 +1,13 @@
+package com.slf4jlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slf4jloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Slf4jloggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slf4jlogging/Slf4jloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slf4jlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slf4jloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
